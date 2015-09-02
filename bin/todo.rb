74d1bb06e42189c516e99b4bea06d858b0587d6e
Travis Montgomery <travisbm@gmail.com>
2015-09-02 17:45:42 -0500

    All tests passing, Program is running - normal mode complete

--- bin/todo.rb
+++ bin/todo.rb
@@ -1,3 +1,14 @@
-require_relative '../lib/todo'
+require_relative '../lib/todo'  # => true
 
-Todo.new('../lib/todos.csv').start
+Todo.new('lib/todos.csv').start  # ~> Errno::ENOENT: No such file or directory @ rb_sysopen - ../lib/todos.csv
+
+# ~> Errno::ENOENT
+# ~> No such file or directory @ rb_sysopen - ../lib/todos.csv
+# ~>
+# ~> /Users/Britton/.rvm/rubies/ruby-2.2.1/lib/ruby/2.2.0/csv.rb:1256:in `initialize'
+# ~> /Users/Britton/.rvm/rubies/ruby-2.2.1/lib/ruby/2.2.0/csv.rb:1256:in `open'
+# ~> /Users/Britton/.rvm/rubies/ruby-2.2.1/lib/ruby/2.2.0/csv.rb:1256:in `open'
+# ~> /Users/Britton/.rvm/rubies/ruby-2.2.1/lib/ruby/2.2.0/csv.rb:1121:in `foreach'
+# ~> /Users/Britton/theironyard/programs/ToDo/todo-csv/lib/todo.rb:9:in `initialize'
+# ~> /Users/Britton/theironyard/programs/ToDo/todo-csv/bin/todo.rb:3:in `new'
+# ~> /Users/Britton/theironyard/programs/ToDo/todo-csv/bin/todo.rb:3:in `<main>'
--- lib/todo.rb
+++ lib/todo.rb
@@ -1,9 +1,11 @@
-require 'csv'
+require 'csv'  # => true
 
 class Todo
 
   def initialize(file_name)
     @file_name = file_name
+    @todos = CSV.read(@file_name, {headers: true})
+
     # You will need to read from your CSV here and assign them to the @todos variable. make sure headers are set to true
   end
 
@@ -13,7 +15,7 @@ class Todo
 
       puts "---- TODO.rb ----"
 
-      view_todos 
+      view_todos
 
       puts
       puts "What would you like to do?"
@@ -35,7 +37,33 @@ class Todo
     @todos
   end
 
-  private
+  def add_todo
+    puts "Name of Todo > "
+    @todos << [get_input, "no"]
+  end
+
+  def mark_todo
+    puts "Which todo have you finished?"
+    num = get_input.to_i
+    @todos[num - 1]["completed"] = "yes"
+  end
+
+  def view_todos
+    puts "Unfinished"
+    @todos.each_with_index do |todo, index|
+      if todo["completed"] == "no"
+      puts "#{index + 1}) #{todo["name"]}"
+      end
+    end
+    puts "Completed"
+    @todos.each_with_index do |todo, index|
+      if todo["completed"] == "yes"
+      puts "#{index + 1}) #{todo["name"]}"
+      end
+    end
+  end
+
+  private        # => Todo
   def get_input
     gets.chomp
   end
